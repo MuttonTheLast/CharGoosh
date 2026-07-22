1cf19d429362cfd7a33eba079d00533eddc0c835
MuttonTheLast <muttonthelast@gmail.com>
2026-07-22 12:41:29 +0330

    Added logger module with modding in mind

---
+++ modules/CharGoosh.Logger/Logger.cs
@@ -0,0 +1,72 @@
+/// why is this a module?
+/// i want other modules have access to it and its a simple init and dispose.
+/// main reason is maybe some mod wants to have customized logger or change main logger
+
+using Serilog;
+
+namespace CharGoosh.Logger;
+
+public class Logger : IDisposable
+{
+    public static LoggerConfiguration DefaultConfig => new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.File("log.txt",
+                rollingInterval: RollingInterval.Hour,
+                rollOnFileSizeLimit: true,
+                fileSizeLimitBytes: 5 * 1024 * 1024); // 5mb each log
+
+
+    public Serilog.Core.Logger Handle { get; private set; }
+    bool _isGlobal = false;
+    bool disposed = false;
+
+    public Logger(LoggerConfiguration? configuration = null)
+    {
+        configuration = configuration == null ? DefaultConfig : configuration;
+        Handle = configuration.CreateLogger();
+    }
+
+    public static implicit operator Serilog.Core.Logger(Logger logger)
+    {
+
+        return logger.Handle;
+    }
+
+    // if true then sets this logger to be global logger
+    public void SetGlobal(bool global = true)
+    {
+        if (global)
+        {
+            Log.Logger = Handle;
+        }
+        else
+        {
+
+            Log.Logger = DefaultConfig.CreateLogger();
+        }
+        _isGlobal = global;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public void Dispose(bool disposing)
+    {
+        if (disposed)
+        {
+            return;
+        }
+        if (disposing)
+        {
+            Handle.Dispose();
+            if (_isGlobal)
+            {
+                SetGlobal(false);
+            }
+        }
+        disposed = true;
+    }
+}
