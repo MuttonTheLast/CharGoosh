c58a708d91bc6716415c841cf03ac9ef28c578ee
MuttonTheLast <muttonthelast@gmail.com>
2026-07-22 12:58:45 +0330

    Added camera
    Camera have 2 variables, one transform and one fov.
    The transform position and rotation is what they should be. The scale is
    not size of camera because camera cant have scale.
    We use scale.X for camera width and Y for height and Z for far plane.
    near plane is 0.1f and fov should be 45 to 120.

---
+++ CharGoosh/Game/Camera.cs
@@ -0,0 +1,55 @@
+
+
+using System;
+using System.Numerics;
+using CharGoosh.Math;
+
+namespace CharGoosh.Game;
+
+public class Camera
+{
+    public static Camera? Main { get; private set; } = null;
+
+    public Transform Transform = new();
+
+    private float _fov = 45;
+    public float FOV
+    {
+        get { return _fov; }
+        set
+        {
+            _fov = value < 45 ? 45 :
+                (value > 120 ? 120 : value);
+        }
+    }
+
+
+    public Matrix4x4 ViewMatrix => Matrix4x4.CreateLookAtLeftHanded(Transform.Position,
+            Transform.Position + Transform.Forward, Transform.Up);
+
+    public Matrix4x4 Projection => Matrix4x4.CreatePerspectiveFieldOfViewLeftHanded(
+            FOV * MathF.PI / 180f,
+            Transform.Scale.X / Transform.Scale.Y, 0.1f,
+            Transform.Scale.Z > 0.2f ? Transform.Scale.Z : 0.2f);
+
+    public Camera() { }
+    public Camera(Transform transform, float fov)
+    {
+        Transform = transform;
+        FOV = fov;
+    }
+
+    ~Camera()
+    {
+        if (Main == this)
+        {
+            Main = null;
+        }
+    }
+
+    public void SetMain()
+    {
+        Main = this;
+    }
+
+}
